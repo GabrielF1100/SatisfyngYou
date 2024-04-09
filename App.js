32e7570a695512818698aa2885e67421c82b8641
IgorPhael <igorraphael@alunos.utfpr.edu.br>
2024-04-09 20:29:49 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View,Text, StyleSheet, Button, TextInput, TouchableOpacity, Image} from 'react-native'
+import { useState } from 'react'
+
+const App = () =>{
+
+  
+
+  return(
+
+    
+  )
+}
+
+export default App;
